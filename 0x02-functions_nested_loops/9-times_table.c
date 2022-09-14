7add1ce02183d535a464ba019120d0b45e32596b
collinsokpowe <collinsokpowe@gmail.com>
2022-09-14 07:07:08 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,12 +2,8 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
-
 {int i, j, k;
-
-
 
 	for (i = 0; i < 10; i++)
 
@@ -34,11 +30,8 @@ void times_table(void)
 			{
 
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar(' ');
-
 				_putchar(k + '0');
 
 			}
@@ -48,11 +41,8 @@ void times_table(void)
 			{
 
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar((k / 10) + '0');
-
 				_putchar((k % 10) + '0');
 
 			}

279124d851950a3f51b30fb33a9e6623bff30c9e
collinsokpowe <collinsokpowe@gmail.com>
2022-09-22 17:30:07 -0700

    task 5 again

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,14 @@ char *string_toupper(char *x)
 {
 	int length;
 
-	length = 0
-		while (x[length] != '\0')
+	length = 0;
+	while (x[length] != '\0')
+	{
+		if (x[length] >= 97 && x[length] <= 122)
 		{
-			if (x[length] >= 97 && x[length] <= 122)
-			{
-				x[length] = x[length] - 32;
-			}
-			length++;
+			x[length] = x[length] - 32;
 		}
+		length++;
+	}
 	return (x);
 }
